bcb054eba86659b3b50b1a912337057fbc8fe8e0
Mark Gilmore <mrkgilmore@gmail.com>
2020-01-27 23:05:52 +0000

    Add user and profile layout added

--- views/modules/404.php
+++ views/modules/404.php
@@ -2,15 +2,6 @@
 
   <section class="content-header">
 
-    
-    <ol class="breadcrumb">
-
-      <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
-
-      <li class="active">404 Page not Found</li>
-
-    </ol>
-
   </section>
 
   <section class="content">
--- views/modules/users.php
+++ views/modules/users.php
@@ -12,19 +12,36 @@
 
     <div class="box">
       <div class="box-header with-border">
+
+      <button class="btn btn-primary" data-toggle="modal" data-target="#addUser">
+
+          Add user
+
+      </button>
       
-        <h3 class="box-title">Title</h3>
-        <div class="box-tools pull-right">
-
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-            <i class="fa fa-minus"></i>
-          </button>
-
-        </div>
+        
       </div>
 
       <div class="box-body">
-      
+        <table class="table table-bordered table-striped dt-responsive tables" width="100%">
+        
+          <thead>
+            
+            <tr>
+              
+              <th style="width:10px">#</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Photo</th>
+              <th>Profile</th>
+              <th>Status</th>
+              <th>Last login</th>
+              <th>Actions</th>
+
+            </tr> 
+
+          </thead>
+        </table>
       </div>
  
       <div class="box-footer">
