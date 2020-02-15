4f9e7024a2be1fb7db26437be84483ccf14215f1
Mark Gilmore <mrkgilmore@gmail.com>
2020-02-15 23:07:01 +0000

    Add categories started with add categories controller done

---
+++ controllers/categories.controller.php
@@ -0,0 +1,64 @@
+<?php
+
+ class ControllerCategories{
+
+    // Create Category
+	
+	static public function CreateCategoryController(){
+
+		if(isset($_POST['newCategory'])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["newCategory"])){
+
+				$table = 'categories';
+
+				$data = $_POST['newCategory'];
+
+				$answer = CategoriesModel::AddCategoryModel($table, $data);
+				// var_dump($answer);
+
+				if($answer == 'ok'){
+
+					echo '<script>
+						
+						swal({
+							type: "success",
+							title: "Category created successfully",
+							showConfirmButton: true,
+							confirmButtonText: "Close"
+
+							}).then(function(result){
+								if (result.value) {
+
+									window.location = "categories";
+
+								}
+							});
+						
+					</script>';
+				}
+				
+
+			}else{
+
+				echo '<script>
+						
+						swal({
+							type: "error",
+							title: "Please fill in all fields, no special characters allowed",
+							showConfirmButton: true,
+							confirmButtonText: "Close"
+				
+							 }).then(function(result){
+
+								if (result.value) {
+									window.location = "categories";
+								}
+							});
+						
+				</script>';
+				
+			}
+		}
+    }
+}
--- views/modules/categories.php
+++ views/modules/categories.php
@@ -43,5 +43,60 @@
 
     </section>
 
-  </div>
+</div>
+
+
+<div id="addCategory" class="modal fade" role="dialog">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <form role="form" method="POST">
+
+                <div class="modal-header" style="background: #3c8dbc; color: #fff">
+
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Add Categories</h4>
+
+                </div>
+
+                <div class="modal-body">
+
+                    <div class="box-body">
+
+                        <div class="form-group">
+
+                            <div class="input-group">
+
+                                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                                <input class="form-control input-lg" type="text" name="newCategory" placeholder="Add Category" required>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save Category</button>
+
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+
+$createCategory = new ControllerCategories();
+$createCategory -> CreateCategoryController();
+?>
 
