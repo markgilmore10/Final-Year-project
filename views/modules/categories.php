38e64687a53f978c4438f3ad64d3e4bb05598767
Mark Gilmore <mrkgilmore@gmail.com>
2020-02-15 22:55:46 +0000

    Categories table added. Same layout as user table

---
+++ views/modules/categories.php
@@ -0,0 +1,47 @@
+<div class="content-wrapper">
+  
+    <section class="content-header">
+
+      <h1>
+        Manage Categories
+      </h1>
+    
+    </section>
+
+ 
+    <section class="content">
+
+      <div class="box">
+
+        <div class="box-header with-border">
+
+          <button class="btn btn-primary" data-toggle="modal" data-target="#addCategories">Add Category</button>
+
+        </div>
+
+        <div class="box-body">
+
+          <table class="table table-bordered table-striped dt-responsive tables" width="100%">
+         
+            <thead>
+             
+             <tr>
+               
+               <th style="width:10px">#</th>
+               <th>Category</th>
+               <th>Actions</th>
+
+             </tr> 
+
+            </thead>
+
+          </table>
+
+        </div>
+      
+      </div>
+
+    </section>
+
+  </div>
+
