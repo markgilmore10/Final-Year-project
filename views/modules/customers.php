b8bbcf597a3663333876bb8bb3e820043b8357b3
Mark Gilmore <mrkgilmore@gmail.com>
2020-03-09 22:08:10 +0000

    Layout for add customer done

--- views/modules/customers.php
+++ views/modules/customers.php
@@ -32,6 +32,7 @@
              
              <th style="width:10px">#</th>
              <th>Name</th>
+             <th>ID</th>
              <th>Address</th>
              <th>Email</th>
              <th>Phone</th>
@@ -58,9 +59,10 @@
               
 
               echo '<tr>
-              
+
                       <td>'.($key+1).'</td>
                       <td>'.$value["name"].'</td>
+                      <td>'.$value["id"].'</td>
                       <td>'.$value["address"].'</td>
                       <td>'.$value["email"].'</td>
                       <td>'.$value["phone"].'</td>
@@ -96,5 +98,117 @@
     </div>
 
   </section>
+
+</div>
+
+<div id="addCustomer" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="POST">
+
+        <div class="modal-header" style="background: #3c8dbc; color: #fff">
+          
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          
+          <h4 class="modal-title">Add Customer</h4>
+
+        </div>
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input class="form-control input-lg" type="text" name="newCustomer" placeholder="Name" required>
+
+              </div>
+
+            </div>
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input class="form-control input-lg" type="number" min="0" name="newId" placeholder="ID Number" required>
+
+              </div>
+
+            </div>
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input class="form-control input-lg" type="text" name="newAddress" placeholder="Address" required>
+
+              </div>
+
+            </div>
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                <input class="form-control input-lg" type="text" name="newEmail" placeholder="Email" required>
+
+              </div>
+
+            </div>
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                <input class="form-control input-lg" type="text" name="newMobile" placeholder="Mobile Number" data-inputmask="'mask':'(999) 999-9999'" data-mask required>
+
+              </div>
+
+            </div>
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                <input class="form-control input-lg" type="text" name="newDob" placeholder="Birth Date" data-inputmask="'alias': 'yyyy/mm/dd'" data-mask required>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+
+        </div>
+
+      </form>
+
+      <?php
+
+        $createCustomer = new CustomersController();
+        $createCustomer -> CreateCustomerController();
+
+      ?>
+      
+    </div>
+
+  </div>
 
 </div>
