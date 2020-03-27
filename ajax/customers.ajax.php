1b5b2d677bb61cc26694cd3b58099f90008e4cde
Mark Gilmore <mrkgilmore@gmail.com>
2020-03-27 00:55:10 +0000

    Search customer by Id more ajax

--- ajax/customers.ajax.php
+++ ajax/customers.ajax.php
@@ -19,12 +19,49 @@ class CustomerAjax{
 
 	}
 
+	public function searchCustomer ($request) {
+        
+        $customers = CustomerController::searchCustomer($request);
+        
+        if ($customers) {
+            
+            foreach ($customers as $customer) {
+
+				echo '<div class="customer_row" data-id="' . $customer->id . '" data-discount="' . $customer->discount . '" >' +
+				
+					 '<div class="name">' . $customer->name . '</div>' +
+					
+					 '<div class="number">' . $customer->idNumber . '</div>' +
+					
+                     '</div>';
+            }
+            
+        } else {
+
+			echo '<div class="customer_row" data-id="">' +
+			
+				 '<div class="name">No data found</div>' +
+				 
+				 '<div class="number"></div>' +
+				 
+                 '</div>';
+        }
+         
+    }
+
 }
 
-if(isset($_POST["idCustomer"])){
+if(isset($_POST["idCustomer"])) {
 
 	$Customer = new CustomerAjax();
 	$Customer -> idCustomer = $_POST["idCustomer"];
 	$Customer -> EditCustomerAjax();
 
+}
+
+if (isset($_POST["search"]) && isset($_POST["number"])) {
+
+    $Customer = new CustomerAjax();
+	$Customer->searchCustomer($_POST["number"]);
+	
 }
