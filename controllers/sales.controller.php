46386ee799038d151dbfd40462f4a86e5db6f5cf
Mark Gilmore <mrkgilmore@gmail.com>
2020-03-28 22:12:59 +0000

    More reopen table

--- controllers/sales.controller.php
+++ controllers/sales.controller.php
@@ -118,6 +118,165 @@ class SalesController{
         // Make Sale
 		if(isset($_POST["reopenSale"])){
 
+			$table = "sales";
+
+			$item = "code";
+			$value = $_POST["reopenSale"];
+
+			$getSale = ModelSales::ShowSalesModel($table, $item, $value);
+
+			if($_POST["productsList"] == ""){
+
+				$productsList = $getSale["products"];
+				$productChange = false;
+
+
+			}else{
+
+				$productsList = $_POST["productsList"];
+				$productChange = true;
+			}
+
+			if($productChange){
+
+				$products =  json_decode($getSale["products"], true);
+
+				$totalPurchasedProducts = array();
+
+				foreach ($products as $key => $value) {
+
+					array_push($totalPurchasedProducts, $value["quantity"]);
+					
+					$tableProducts = "products";
+
+					$item = "id";
+					$value = $value["id"];
+					$order = "id";
+
+					$getProduct = ProductsModel::mdlShowProducts($tableProducts, $item, $value, $order);
+
+					$item1a = "sales";
+					$value1a = $getProduct["sales"] - $value["quantity"];
+
+					$newSales = ProductsModel::mdlUpdateProduct($tableProducts, $item1a, $value1a, $value);
+
+					$item1b = "stock";
+					$value1b = $value["quantity"] + $getProduct["stock"];
+
+					$nuevoStock = ProductsModel::mdlUpdateProduct($tableProducts, $item1b, $value1b, $value);
+
+				}
+
+				$tableCustomers = "customers";
+
+				$itemCustomer = "id";
+				$valueCustomer = $_POST["selectCustomer"];
+
+				$getCustomer = ModelCustomers::mdlShowCustomers($tableCustomers, $itemCustomer, $valueCustomer);
+
+				$item1a = "purchases";
+				$value1a = $getCustomer["purchases"] - array_sum($totalPurchasedProducts);
+
+				$customerPurchases = ModelCustomers::mdlUpdateCustomer($tableCustomers, $item1a, $value1a, $valueCustomer);
+
+				/*=============================================
+				UPDATE THE CUSTOMER'S PURCHASES AND REDUCE THE STOCK AND INCREMENT PRODUCT SALES
+				=============================================*/
+
+				$productsList_2 = json_decode($productsList, true);
+
+				$totalPurchasedProducts_2 = array();
+
+				foreach ($productsList_2 as $key => $value) {
+
+					array_push($totalPurchasedProducts_2, $value["quantity"]);
+					
+					$tableProducts_2 = "products";
+
+					$item_2 = "id";
+					$value_2 = $value["id"];
+					$order = "id";
+
+					$getProduct_2 = ProductsModel::ShowProductsModel($tableProducts_2, $item_2, $value_2, $order);
+
+					$item1a_2 = "sales";
+					$value1a_2 = $value["quantity"] + $getProduct_2["sales"];
+
+					$newSales_2 = ProductsModel::UpdateProductModel($tableProducts_2, $item1a_2, $value1a_2, $value_2);
+
+					$item1b_2 = "stock";
+					$value1b_2 = $getProduct_2["stock"] - $value["quantity"];
+
+					$newStock_2 = ProductsModel::UpdateProductModel($tableProducts_2, $item1b_2, $value1b_2, $value_2);
+
+				}
+
+				$tableCustomers_2 = "customers";
+
+				$item_2 = "id";
+				$value_2 = $_POST["selectCustomer"];
+
+				$getCustomer_2 = CustomersModel::ShowCustomersModel($tableCustomers_2, $item_2, $value_2);
+
+				$item1a_2 = "purchases";
+				$value1a_2 = array_sum($totalPurchasedProducts_2) + $getCustomer_2["purchases"];
+
+				$customerPurchases_2 = ModelCustomers::mdlUpdateCustomer($tableCustomers_2, $item1a_2, $value1a_2, $value_2);
+
+				$item1b_2 = "lastPurchase";
+
+				date_default_timezone_set("Europe/Dublin");
+
+				$date = date('Y-m-d');
+				$hour = date('H:i:s');
+				$value1b = $date.' '.$hour;
+
+				$dateCustomer_2 = CustomersModel::UpdateCustomerModel($tableCustomers_2, $item1b_2, $value1b_2, $value_2);
+
+			}
+
+			/*=============================================
+			SAVE PURCHASE CHANGES
+			=============================================*/	
+
+			$data = array("code"=>$_POST["reopenSale"],
+						  "idSeller"=>$_POST["idSeller"],
+						  "tableNo"=>$_POST["tableNo"],
+						  "idCustomer"=>$_POST["customerSearch"],
+						  "products"=>$_POST["productsList"],
+						  "netPrice"=>$_POST["newNetPrice"],
+						  "discount"=>$_POST["newDiscountPrice"],
+						  "totalPrice"=>$_POST["newSaleTotal"],
+						  "paymentMethod"=>$_POST["newPaymentMethod"]);
+
+			$answer = ModelSales::AddSaleModel($table, $data);
+
+			if($answer == "ok"){
+
+				echo'<script>
+
+				localStorage.removeItem("range");
+
+				swal({
+					  type: "success",
+					  title: "Sale Succesfull",
+					  showConfirmButton: true,
+					  confirmButtonText: "Close"
+					  }).then((result) => {
+								if (result.value) {
+
+								window.location = "sales";
+
+								}
+							})
+
+				</script>';
+
+			}
+		}
+
+	}
+
 			$productsList = json_decode($_POST["productsList"], true);
 
 			$totalPurchase = array();
@@ -180,7 +339,7 @@ class SalesController{
 						  "totalPrice"=>$_POST["newSaleTotal"],
 						  "paymentMethod"=>$_POST["newPaymentMethod"]);
 
-			$answer = ModelSales::AddSaleModel($table, $data);
+			$answer = ModelSales::ReopenSaleModel($table, $data);
 
 			if($answer == "ok"){
 
