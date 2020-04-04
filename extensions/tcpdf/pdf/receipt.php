8c7584bda27344863c1f2af5251d9194303132dc
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-04-04 17:17:24 +0100

    titles showing pdf format started

--- extensions/tcpdf/pdf/receipt.php
+++ extensions/tcpdf/pdf/receipt.php
@@ -7,4 +7,56 @@ $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8',
 $pdf->startPageGroup();
 
 $pdf->AddPage();
+
+$block1 = <<<EOF
+
+	<table>
+		
+		<tr>
+			
+			<td style="width:150px"><img src="images/logo-cellar.png"></td>
+
+			<td style="background-color:white; width:140px">
+				
+				<div style="font-size:8.5px; text-align:right; line-height:15px;">
+					
+					<br>
+					ADDRESS: 12 Eglinton St, Galway
+
+                    <br>
+                    EIRCODE: H91 D278
+					
+				</div>
+
+			</td>
+
+			<td style="background-color:white; width:140px">
+
+				<div style="font-size:8.5px; text-align:right; line-height:15px;">
+					
+					<br>
+					PHONE: (091) 563 966
+					
+					<br>
+					thecellar.ie
+
+				</div>
+				
+			</td>
+
+			<td style="background-color:white; width:110px; text-align:center; color:red"><br><br>Rec N.<br></td>
+
+		</tr>
+
+	</table>
+
+EOF;
+
+$pdf->writeHTML($block1, false, false, false, false, '');
+
+
+ob_end_clean();
+$pdf->Output('receipt.pdf');
+
+
 ?>
