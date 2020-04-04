815de42a4c44b4dcc79869de98de8255543108fc
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-04-04 19:36:43 +0100

    added customer/date to pdf

--- extensions/tcpdf/pdf/receipt.php
+++ extensions/tcpdf/pdf/receipt.php
@@ -3,16 +3,44 @@
 require_once "../../../controllers/sales.controller.php";
 require_once "../../../models/sales.model.php";
 
+require_once "../../../controllers/customers.controller.php";
+require_once "../../../models/customer.model.php";
+
+require_once "../../../controllers/users.controller.php";
+require_once "../../../models/users.model.php";
+
+require_once "../../../controllers/products.controller.php";
+require_once "../../../models/products.model.php";
+
 class printReceipt{
 
 public $code;
 
 public function getReceiptPrinting(){
 
+// Sale Info
 $itemSale = "code";
 $saleValue = $this->code;
     
 $answerSale = SalesController::ShowSalesController($itemSale, $saleValue);
+
+$saledate = substr($answerSale["saledate"],0,-8);
+$products = json_decode($answerSale["products"], true);
+$netPrice = number_format($answerSale["netPrice"],2);
+$tax = number_format($answerSale["tax"],2);
+$totalPrice = number_format($answerSale["totalPrice"],2);
+
+// Customer Info
+$itemCustomer = "id";
+$CustomerValue = $answerSale["customerId"];
+
+$answerCustomer = CustomerController::ShowCustomerController($itemCustomer, $valueCustomer);
+
+//User Info
+$itemUser = "id";
+$userValue = $answerSale["userId"];
+
+$answerUser = ControllerUsers::ShowUsers($itemUser, $userValue);
 
 
 
@@ -70,6 +98,33 @@ EOF;
 
 $pdf->writeHTML($block1, false, false, false, false, '');
 
+//------------------------------------------------------------------------------------------------------//
+
+$block2 = <<<EOF
+
+    <table style="font-size:10px; padding:5px 10px;">
+
+        <tr>
+
+            <td style="border: 1px solid #666; background-color:white; width:390px">
+
+				Customer: $answerCustomer[name]
+
+            </td>
+
+            <td style="border: 1px solid #666; background-color:white; width:150px; text-align:right">
+			
+				Date: $saledate
+
+			</td>
+            
+        </tr>
+    
+    </table>
+
+EOF;
+
+$pdf->writeHTML($block2, false, false, false, false, '');
 
 ob_end_clean();
 $pdf->Output('receipt.pdf');
