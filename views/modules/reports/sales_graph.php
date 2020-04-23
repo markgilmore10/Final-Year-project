19e7391cd83a28683355059ce8c20068f4026f03
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-04-23 18:47:27 +0100

    salesgraph_line template added

--- views/modules/reports/sales_graph.php
+++ views/modules/reports/sales_graph.php
@@ -44,3 +44,35 @@ foreach ($answer as $key => $value) {
     </div>
 
 </div>
+
+<script>
+  var line = new Morris.Line({
+    element          : 'Sales-line-chart',
+    resize           : true,
+    data             : [
+      { y: '2011 Q1', item1: 2666 },
+      { y: '2011 Q2', item1: 2778 },
+      { y: '2011 Q3', item1: 4912 },
+      { y: '2011 Q4', item1: 3767 },
+      { y: '2012 Q1', item1: 6810 },
+      { y: '2012 Q2', item1: 5670 },
+      { y: '2012 Q3', item1: 4820 },
+      { y: '2012 Q4', item1: 15073 },
+      { y: '2013 Q1', item1: 10687 },
+      { y: '2013 Q2', item1: 8432 }
+    ],
+    xkey             : 'y',
+    ykeys            : ['item1'],
+    labels           : ['Item 1'],
+    lineColors       : ['#efefef'],
+    lineWidth        : 2,
+    hideHover        : 'auto',
+    gridTextColor    : '#fff',
+    gridStrokeWidth  : 0.4,
+    pointSize        : 4,
+    pointStrokeColors: ['#efefef'],
+    gridLineColor    : '#efefef',
+    gridTextFamily   : 'Open Sans',
+    gridTextSize     : 10
+  });
+</script>
