3060db84e49ffa2f116b14ee1e08eb6de97edbb9
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-04-24 11:15:03 +0100

    template foe best sellers

---
+++ views/modules/reports/bestselling_products.php
@@ -0,0 +1,92 @@
+<div class="box box-default">
+
+    <div class="box-header with-border">
+
+        <h3 class="box-title">Browser Usage</h3>
+
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-card-widget="collapse">
+            
+                <i class="fa fa-minus"></i>
+            
+            </button>
+
+            <button type="button" class="btn btn-box-tool" data-card-widget="remove">
+
+                <i class="fa fa-times"></i>
+
+            </button>
+        </div>
+    </div>
+
+
+    <div class="box-body">
+
+        <div class="row">
+
+            <div class="col-md-8">
+
+                <div class="chart-responsive">
+
+                    <canvas id="pieChart" height="150"></canvas>
+
+                </div>
+       
+            </div>
+      
+        <div class="col-md-4">
+        
+            <ul class="chart-legend clearfix">
+
+                <li><i class="fa fa-circle-o text-red"></i> Chrome</li>
+                <li><i class="fa fa-circle-o text-green"></i> IE</li>
+                <li><i class="fa fa-circle-o text-yellow"></i> FireFox</li>
+                <li><i class="fa fa-circle-o text-aqua"></i> Safari</li>
+                <li><i class="fa fa-circle-o text-light-blue"></i> Opera</li>
+                <li><i class="fa fa-circle-o text-gray"></i> Navigator</li>
+
+            </ul>
+
+        </div>
+       
+    </div>
+   
+</div>
+
+    <div class="box-footer no-padding">
+
+        <ul class="nav nav-pills nav-stacked">
+
+            <li>
+            
+            <a href="#">
+                United States of America
+                <span class="pull-right text-red">
+                <i class="fa fa-angle-down"></i>
+                12%</span>
+            </a>
+            </li>
+
+            <li>
+            <a href="#">
+                India
+                <span class="pull-right text-green">
+                <i class="fa fa-angle-up"></i> 4%
+                </span>
+            </a>
+            </li>
+
+            <li>
+            <a href="#">
+                China
+                <span class="pull-right text-yellow">
+                <i class="fa fa-angle-left"></i> 0%
+                </span>
+            </a>
+            </li>
+
+        </ul>
+
+    </div>
+
+</div>
