a1bca660dfabc8256fbb47708001b4570f6bffbe
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-05-03 10:47:03 +0100

    dashboard boxviews

--- views/modules/dashboard.php
+++ views/modules/dashboard.php
@@ -6,31 +6,26 @@
       Dashboard
     </h1>
 
+    <ol class="breadcrumb">
+
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+
+      <li class="active">Dashboard</li>
+
+    </ol>
+
   </section>
 
   <section class="content">
 
-    <div class="box">
-      <div class="box-header with-border">
-      
-        <h3 class="box-title">Title</h3>
-        <div class="box-tools pull-right">
+    <div class ="row">
 
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-            <i class="fa fa-minus"></i>
-          </button>
+      <?php
+    
+        include "dashboard/box_views.php"
 
-        </div>
-      </div>
-
-      <div class="box-body">
-      
-      </div>
- 
-      <div class="box-footer">
-      
-      </div>
-
+      ?>
+    
     </div>
 
   </section>
