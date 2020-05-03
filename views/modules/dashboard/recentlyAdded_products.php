f5ace2994d8f8a8413a01e6698d17896f38bb5d8
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-05-03 12:33:34 +0100

    recently added products

--- views/modules/dashboard/recentlyAdded_products.php
+++ views/modules/dashboard/recentlyAdded_products.php
@@ -1,10 +1,20 @@
+<?php
+
+$item = null;
+$value = null;
+$order = "id";
+
+$products = ProductsController::ShowProductsController($item, $value, $order);
+
+ ?>
+
 <div class="box box-primary">
 
     <div class="box-header with-border">
 
         <h3 class="box-title">Recently Added Products</h3>
 
-        <div class="box-tools pull-right">#
+        <div class="box-tools pull-right">
 
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
             </button>
@@ -16,93 +26,35 @@
     <div class="box-body">
 
         <ul class="products-list product-list-in-box">
-        
-            <li class="item">
 
-                <div class="product-img">
+        <?php
 
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
+        for($i = 0; $i < 10; $i++){
 
-                </div>
+            echo '<li class="item">
 
                 <div class="product-info">
 
-                    <a href="javascript:void(0)" class="product-title">Samsung TV
-                    <span class="label label-warning pull-right">$1800</span></a>
-                    <span class="product-description">
-                    Samsung 32" 1080p 60Hz LED Smart HDTV.
-                    </span>
+                    <a href="" class="product-title">
+
+                        '.$products[$i]["product"].'
+
+                        <span class="label label-warning pull-right">€'.$products[$i]["sellingPrice"].'</span>
+                        
+                    </a>
 
                 </div>
 
-            </li>
-
-            <li class="item">
-
-                <div class="product-img">
-
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-
-                </div>
-
-                <div class="product-info">
-
-                    <a href="javascript:void(0)" class="product-title">Bicycle
-                    <span class="label label-info pull-right">$700</span></a>
-                    <span class="product-description">
-                    26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-                    </span>
-
-                </div>
-
-            </li>
-
-            <li class="item">
-
-                <div class="product-img">
-
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-
-                </div>
-
-                <div class="product-info">
-
-                    <a href="javascript:void(0)" class="product-title">Xbox One <span
-                    class="label label-danger pull-right">$350</span></a>
-                    <span class="product-description">
-                    Xbox One Console Bundle with Halo Master Chief Collection.
-                    </span>
-
-                </div>
-
-            </li>
-
-            <li class="item">
-
-                <div class="product-img">
-                    
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-                
-                </div>
-                
-                <div class="product-info">
-            
-                    <a href="javascript:void(0)" class="product-title">PlayStation 4
-                    <span class="label label-success pull-right">$399</span></a>
-                    <span class="product-description">
-                    PlayStation 4 500GB Console (PS4)
-                    </span>
-                
-                </div>
-
-            </li>
+            </li>';
+        }
+        ?>
 
         </ul>
     </div>
 
     <div class="box-footer text-center">
     
-    <a href="javascript:void(0)" class="uppercase">View All Products</a>
+    <a href="products" class="uppercase">View All Products</a>
     
     </div>
 
