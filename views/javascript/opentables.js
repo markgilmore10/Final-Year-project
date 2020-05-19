3169f4befc584b8f963b1a827c9d9f0a3d69fef6
Mark Gilmore <mrkgilmore@gmail.com>
2020-05-19 01:02:43 +0100

    Delete sale

--- views/javascript/opentables.js
+++ views/javascript/opentables.js
@@ -6,3 +6,25 @@ $(".tables").on("click", ".btnReopenTable", function(){
     window.location = "index.php?route=reopen-table&id="+idSale;
     
 })
+
+$(".tables").on("click", ".btnDeleteTable", function(){
+
+	var idSale = $(this).attr("idSale");
+  
+	swal({
+		  title: 'Delete Table?',
+		  type: 'warning',
+		  showCancelButton: true,
+		  confirmButtonColor: '#3085d6',
+		  cancelButtonColor: '#d33',
+		  cancelButtonText: 'Cancel',
+		  confirmButtonText: 'Yes'
+		}).then(function(result){
+		  if (result.value) {
+			
+			  window.location = "index.php?route=open-tables&idSale="+idSale;
+		  }
+  
+	})
+  
+  })
