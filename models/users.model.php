b30209bbf235fa4fc11570ac177cf8d10b234cbc
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2020-05-21 21:12:41 +0100

    usersML docs

--- models/users.model.php
+++ models/users.model.php
@@ -2,9 +2,21 @@
 
 require_once "connection.php";
 
+/**
+ * Class UserModel
+ */
 class UserModel{
 
 	// Show User
+	/**
+	 * shows the selected user from the user table in the database or displays all users in the table
+	 * 
+	 * @param mixed $table
+	 * @param mixed $item
+	 * @param mixed $value
+	 * 
+	 * @return void
+	 */
 	public static function ShowUsersModel($table, $item, $value){
 
 		if($item != null){
@@ -32,6 +44,13 @@ class UserModel{
 
 	}
 
+	/**
+	 * adds users into the users table in the database using input data
+	 * @param mixed $table
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
 	public static function AddUserModel($table, $data){
 
 		$stmt = Connection::connect()->prepare("INSERT INTO $table(name, user, password, profile) VALUES (:name, :user, :password, :profile)");
@@ -55,6 +74,14 @@ class UserModel{
 		$stmt = null;
 	}
 
+	/**
+	 * edits the user in the users table using input data
+	 * 
+	 * @param mixed $table
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
 	public static function EditUserModel($table, $data){
 
 		$stmt = Connection::connect()->prepare("UPDATE $table set name = :name, password = :password, profile = :profile WHERE user = :user");
@@ -79,6 +106,17 @@ class UserModel{
 		$stmt = null;
 	}
 
+	/**
+	 * updates the user table using input data
+	 * 
+	 * @param mixed $table
+	 * @param mixed $item1
+	 * @param mixed $value1
+	 * @param mixed $item2
+	 * @param mixed $value2
+	 * 
+	 * @return void
+	 */
 	public static function UpdateUserModel($table, $item1, $value1, $item2, $value2){
 
 		$stmt = Connection::connect()->prepare("UPDATE $table set $item1 = :$item1 WHERE $item2 = :$item2");
@@ -101,6 +139,13 @@ class UserModel{
 		$stmt = null;
 	}
 
+	/**
+	 * deletes selected user from the user table in the database by id
+	 * @param mixed $table
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
 	public static function DeleteUserModel($table, $data){
 
 		$stmt = Connection::connect()->prepare("DELETE FROM $table WHERE id = :id");
